fbf01bff31232468aeab891d6e5136d16159b165
きのした <kyousuke.kinoshita@gmail.com>
2018-06-18 14:31:06 +0000

    6.25 done

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -28,13 +28,51 @@ RSpec.describe User, type: :model do
   it "is invalid unless name is within 50 characters" do
     @user.name = "a" * 51
     @user.valid?
-    expect(@user.errors[:name]).to include("is too long")
+    expect(@user.errors[:name]).to include("is too long (maximum is 50 characters)")
   end
   
   # メールアドレスが255文字以内でなければ無効な状態であること
   it "is invalid unless email is within 255 characters" do
     @user.email = "a" * 244 + "@example.com"
     @user.valid?
-    expect(@user.errors[:email]).to include("is too long")
+    expect(@user.errors[:email]).to include("is too long (maximum is 255 characters)")
+  end
+  
+  # メールアドレスが有効なフォーマットであれば有効な状態であること
+  it "is valid with email is valid format" do
+    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+      first.last@foo.jp alice+bob@baz.cn]
+     valid_addresses.each do |valid_address|
+       @user.email = valid_address
+       expect(@user).to be_valid
+     end
+  end
+  
+  # メールアドレスが有効なフォーマットでなければ無効な状態であること
+  it "is invalid unless email is valid format" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+      foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      @user.valid?
+      expect(@user.errors[:email]).to include("is invalid")
+    end
+  end
+  
+  # メールアドレスが一意なら有効な状態であること
+  it "is valid with unique email" do
+    duplicate_user = @user.dup
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    duplicate_user.valid?
+    expect(duplicate_user.errors[:email]).to include("has already been taken")
+  end
+  
+  # メールアドレスが大文字・小文字混在でも登録時に小文字に変換されること
+  example " email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    expect(mixed_case_email.downcase).to eq @user.reload.email 
   end
 end
