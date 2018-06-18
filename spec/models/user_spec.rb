9ecc5d9778941eb7646a19ce1643b4665380158f
きのした <kyousuke.kinoshita@gmail.com>
2018-06-18 00:57:28 +0000

    6.2.3 done

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -23,4 +23,18 @@ RSpec.describe User, type: :model do
     @user.valid?
     expect(@user.errors[:email]).to include("can't be blank")
   end
+  
+  # 名前が50文字以内でなければ無効な状態であること
+  it "is invalid unless name is within 50 characters" do
+    @user.name = "a" * 51
+    @user.valid?
+    expect(@user.errors[:name]).to include("is too long")
+  end
+  
+  # メールアドレスが255文字以内でなければ無効な状態であること
+  it "is invalid unless email is within 255 characters" do
+    @user.email = "a" * 244 + "@example.com"
+    @user.valid?
+    expect(@user.errors[:email]).to include("is too long")
+  end
 end
