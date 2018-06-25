85f5e8f3a13a8b21661b1290b7ba87d694fc79a8
きのした <kyousuke.kinoshita@gmail.com>
2018-06-25 00:47:22 +0000

    8.2.2 done

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,12 @@
 module SessionsHelper
+  
+  # 渡されたユーザでろぐいんする
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+  
+  # 現在ログイン中のユーザを返却する
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
 end
